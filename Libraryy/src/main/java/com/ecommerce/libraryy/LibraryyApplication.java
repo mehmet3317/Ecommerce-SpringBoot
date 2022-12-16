4a0369b873da2191c71b586d5167356ebbcead25
mehmet3317 <mehmet95ozturk@gmail.com>
2022-12-16 22:45:19 +0100

    Initial commit

---
+++ Libraryy/src/main/java/com/ecommerce/libraryy/LibraryyApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.libraryy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryyApplication.class, args);
+    }
+
+}
---
+++ Libraryy/src/test/java/com/ecommerce/libraryy/LibraryyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.libraryy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
