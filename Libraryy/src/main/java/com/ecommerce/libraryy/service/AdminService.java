f9bd2d12d1eed1799adf90dc39cfa99aaf20ad1a
mehmet3317 <mehmet95ozturk@gmail.com>
2022-12-19 23:49:10 +0100

    AdminService was implemented.

--- Libraryy/src/main/java/com/ecommerce/libraryy/service/AdminService.java
+++ Libraryy/src/main/java/com/ecommerce/libraryy/service/AdminService.java
@@ -1,4 +1,11 @@
 package com.ecommerce.libraryy.service;
 
+import com.ecommerce.libraryy.dto.AdminDto;
+import com.ecommerce.libraryy.model.Admin;
+
 public interface AdminService {
+
+    Admin findByUsername(String username);
+
+    Admin save(AdminDto adminDto);
 }
---
+++ Libraryy/src/main/java/com/ecommerce/libraryy/service/impl/AdminServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ecommerce.libraryy.service.impl;
+
+import com.ecommerce.libraryy.dto.AdminDto;
+import com.ecommerce.libraryy.model.Admin;
+import com.ecommerce.libraryy.repository.AdminRepository;
+import com.ecommerce.libraryy.repository.RoleRepository;
+import com.ecommerce.libraryy.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Service
+public class AdminServiceImpl implements AdminService {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public Admin findByUsername(String username) {
+        return adminRepository.findByUsername(username);
+    }
+
+    @Override
+    public Admin save(AdminDto adminDto) {
+        Admin admin = new Admin();
+        admin.setFirstName(adminDto.getFirstName());
+        admin.setLastName(adminDto.getLastName());
+        admin.setUsername(adminDto.getUsername());
+        admin.setPassword(admin.getPassword());
+        admin.setRoles(Arrays.asList(roleRepository.findByName("ADMIN")));
+        return adminRepository.save(admin);
+    }
+}
