4c3333eb3e069baf53a9e81c44a2eabb45c04aad
mehmet3317 <mehmet95ozturk@gmail.com>
2022-12-20 22:05:51 +0100

    application.properties was configured.

--- Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
+++ Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
@@ -2,8 +2,12 @@ package com.ecommerce.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ecommerce.libraryy.*")
+@EnableJpaRepositories(value = "com.ecommerce.libraryy.repository")
+@EntityScan(value = "com.ecommerce.libraryy.model")
 public class AdminApplication {
 
 	public static void main(String[] args) {
