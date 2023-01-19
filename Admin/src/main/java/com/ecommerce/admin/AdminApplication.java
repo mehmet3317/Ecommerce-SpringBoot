b3ef903d233b05ed2410a6823579598c0386eb9c
mehmet3317 <mehmet95ozturk@gmail.com>
2023-01-19 23:10:37 +0100

    Login controller and login.html were created and implemented

--- Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
+++ Admin/src/main/java/com/ecommerce/admin/AdminApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.ecommerce.libraryy.*")
+@SpringBootApplication(scanBasePackages = {"com.ecommerce.libraryy.*", "com.ecommerce.admin.*"})
 @EnableJpaRepositories(value = "com.ecommerce.libraryy.repository")
 @EntityScan(value = "com.ecommerce.libraryy.model")
 public class AdminApplication {
