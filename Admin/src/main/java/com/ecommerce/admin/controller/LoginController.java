75930be474de17b251d338355a30f7272f63e336
mehmet3317 <mehmet95ozturk@gmail.com>
2023-01-22 23:57:57 +0100

    update on LoginController

--- Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.admin.controller;
 
+import com.ecommerce.libraryy.dto.AdminDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,5 +11,18 @@ public class LoginController {
     @GetMapping("/login")
     public String loginForm(){
         return "Login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model){
+        model.addAttribute("title", "Register");
+        model.addAttribute("adminDto", new AdminDto());
+        return "register";
+    }
+
+    @GetMapping("/forgot-password")
+    public String forgotPassword(Model model){
+        model.addAttribute("title", "Forgot Password");
+        return "forgot-password";
     }
 }
