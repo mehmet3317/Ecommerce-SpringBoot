54aa44223fc4e382f6d49233f66998d1e1548a68
mehmet3317 <mehmet95ozturk@gmail.com>
2023-01-24 17:30:36 +0100

    update on register.html

--- Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
+++ Admin/src/main/java/com/ecommerce/admin/controller/LoginController.java
@@ -1,28 +1,69 @@
 package com.ecommerce.admin.controller;
 
 import com.ecommerce.libraryy.dto.AdminDto;
+import com.ecommerce.libraryy.model.Admin;
+import com.ecommerce.libraryy.service.impl.AdminServiceImpl;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.naming.Binding;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private AdminServiceImpl adminService;
 
     @GetMapping("/login")
     public String loginForm(){
         return "Login";
     }
-
     @GetMapping("/register")
     public String register(Model model){
         model.addAttribute("title", "Register");
         model.addAttribute("adminDto", new AdminDto());
         return "register";
     }
-
     @GetMapping("/forgot-password")
     public String forgotPassword(Model model){
         model.addAttribute("title", "Forgot Password");
         return "forgot-password";
+    }
+    @PostMapping("/register-new")
+    public String addNewAdmin(@Valid @ModelAttribute("adminDto")AdminDto adminDto,
+                              BindingResult result,
+                              Model model,
+                              RedirectAttributes redirectAttributes){
+        try{
+            if(result.hasErrors()){
+                model.addAttribute("adminDto",adminDto);
+                return "register";
+            }
+            String username = adminDto.getUsername();
+            Admin admin = adminService.findByUsername(username);
+            if(admin != null){
+                redirectAttributes.addFlashAttribute("message","Your email has been registered");
+                return "register";
+            }
+            if(adminDto.getPassword().equals(adminDto.getRepeatPassword())){
+                adminService.save(adminDto);
+                model.addAttribute("adminDto",adminDto);
+                redirectAttributes.addFlashAttribute("message","registered successfully");
+            }else{
+                model.addAttribute("adminDto",adminDto);
+                redirectAttributes.addFlashAttribute("message","password is not same");
+                return "register";
+            }
+        }catch (Exception e){
+            redirectAttributes.addFlashAttribute("message","Can not registered because error server");
+        }
+
+        return "register";
     }
 }
--- Libraryy/src/main/java/com/ecommerce/libraryy/service/impl/AdminServiceImpl.java
+++ Libraryy/src/main/java/com/ecommerce/libraryy/service/impl/AdminServiceImpl.java
@@ -6,6 +6,7 @@ import com.ecommerce.libraryy.repository.AdminRepository;
 import com.ecommerce.libraryy.repository.RoleRepository;
 import com.ecommerce.libraryy.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Array;
@@ -13,6 +14,8 @@ import java.util.Arrays;
 
 @Service
 public class AdminServiceImpl implements AdminService {
+
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private AdminRepository adminRepository;
@@ -31,7 +34,7 @@ public class AdminServiceImpl implements AdminService {
         admin.setFirstName(adminDto.getFirstName());
         admin.setLastName(adminDto.getLastName());
         admin.setUsername(adminDto.getUsername());
-        admin.setPassword(admin.getPassword());
+        admin.setPassword(passwordEncoder.encode(adminDto.getPassword()));
         admin.setRoles(Arrays.asList(roleRepository.findByName("ADMIN")));
         return adminRepository.save(admin);
     }
